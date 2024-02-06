56bb5292501ee6186ed4f82c1635ba0e89bad5f6
Marshall-Bits <kz_@hotmail.es>
2024-02-06 14:55:19 +0100

    iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import LikeButton from "./components/LikeButton";
+import Counter from "./components/Counter";
+import ClickablePicture from "./components/ClickablePicture";
 
 function App() {
   return (
     <div className="App">
-      <h1> LAB | React Training</h1>
+      <h1> LAB | React Bacon Training</h1>
+      <LikeButton />
+      <LikeButton />
+      <br />
+      <Counter />
+      <br />
+      <ClickablePicture />
+
     </div>
   );
 }
